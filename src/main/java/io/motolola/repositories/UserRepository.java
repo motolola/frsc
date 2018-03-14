8bc803b0e5480017e88f6288a52eeb15af4e4a39
motolola <motolola@icloud.com>
2018-03-14 15:15:49 +0000

    changed to JpaRepository for more options ..

--- src/main/java/io/motolola/repositories/UserRepository.java
+++ src/main/java/io/motolola/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package io.motolola.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import io.motolola.models.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findOneByUserName(String name);
     User findOneByEmail(String email);
     User findOneByUserNameOrEmail(String username, String email);
