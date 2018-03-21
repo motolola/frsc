e0e41b2f7fbabc8b44405494fa497813fc35c4bc
motolola <motolola@icloud.com>
2018-03-21 17:14:58 +0000

    [Heroku - Issue] issue

--- src/main/java/ng/gov/frsc/models/MemberMessage.java
+++ src/main/java/ng/gov/frsc/models/MemberMessage.java
@@ -2,15 +2,27 @@ package ng.gov.frsc.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 public class MemberMessage {
-	 @GeneratedValue
-	    @Id
-	    private Long id;
+	 //@GeneratedValue
+	   // @Id
+	    //private Long id;
+	@Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "enhanced_table_seq_gen")
+    @GenericGenerator(name = "enhanced_table_seq_gen", strategy = "org.hibernate.id.enhanced.TableGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "table_name", value = "lib_sequences"),
+                    @org.hibernate.annotations.Parameter(name = "segment_column_name", value = "name"),
+                    @org.hibernate.annotations.Parameter(name = "segment_value", value = "users_seq"),
+                    @org.hibernate.annotations.Parameter(name = "value_column_name", value = "next_val")})
+        private Long id;
 	    
 	    @NotNull
 	    @Size(min = 3, max = 100, message = "Subject must at least 3 characters.")
--- src/main/java/ng/gov/frsc/models/Model.java
+++ src/main/java/ng/gov/frsc/models/Model.java
@@ -4,15 +4,28 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.Version;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @MappedSuperclass
 public abstract class Model {
-    @GeneratedValue
-    @Id
+    //@GeneratedValue
+    //@Id
+    //private Long id;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "enhanced_table_seq_gen")
+    @GenericGenerator(name = "enhanced_table_seq_gen", strategy = "org.hibernate.id.enhanced.TableGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "table_name", value = "lib_sequences"),
+                    @org.hibernate.annotations.Parameter(name = "segment_column_name", value = "name"),
+                    @org.hibernate.annotations.Parameter(name = "segment_value", value = "users_seq"),
+                    @org.hibernate.annotations.Parameter(name = "value_column_name", value = "next_val")})
     private Long id;
 
     private Date dateCreated;
