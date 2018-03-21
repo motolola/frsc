e61bcb669f33cac41fb673bdf8acd0a602273274
motolola <motolola@icloud.com>
2018-03-21 17:04:12 +0000

    [Heroku - Issue] issue

--- src/main/java/ng/gov/frsc/models/User.java
+++ src/main/java/ng/gov/frsc/models/User.java
@@ -2,17 +2,30 @@ package ng.gov.frsc.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.hibernate.annotations.GenericGenerator;
 
 import org.hibernate.validator.constraints.Email;
 
+
 @Entity
 public class User {
-    @GeneratedValue
-    @Id
+    //@GeneratedValue
+    //@Id
+    //private Long id;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "enhanced_table_seq_gen")
+    @GenericGenerator(name = "enhanced_table_seq_gen", strategy = "org.hibernate.id.enhanced.TableGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "table_name", value = "lib_sequences"),
+                    @org.hibernate.annotations.Parameter(name = "segment_column_name", value = "name"),
+                    @org.hibernate.annotations.Parameter(name = "segment_value", value = "users_seq"),
+                    @org.hibernate.annotations.Parameter(name = "value_column_name", value = "next_val")})
     private Long id;
     
     @NotNull
