43f2dccc26bfe685363dacbfd44842f19ce359e7
motolola <motolola@icloud.com>
2018-03-23 23:56:29 +0000

    Offence Template

--- src/main/java/ng/gov/frsc/controllers/OffenceController.java
+++ src/main/java/ng/gov/frsc/controllers/OffenceController.java
@@ -1,8 +1,16 @@
 package ng.gov.frsc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("offence")
 public class OffenceController {
+	@GetMapping("my-offence")
+	public String myOffence()
+	{
+		return "offence/my_offence";
+	}
 
 }
