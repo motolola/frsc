325cff128078a5aec9a9611c0beca435782afbfa
motolola <motolola@icloud.com>
2018-03-23 00:12:15 +0000

    Vehicle list

--- src/main/java/ng/gov/frsc/models/Vehicle.java
+++ src/main/java/ng/gov/frsc/models/Vehicle.java
@@ -10,10 +10,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Table(name="vehicle")
 public class Vehicle extends Model {
 	
 	@Id
